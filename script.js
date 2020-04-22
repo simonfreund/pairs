fbf84a313ba3efe69c10b3fb4deb9fc7beb18f88
marcellocurto <marcello@roark.at>
2020-04-22 21:57:14 +0200

    V2 🧞‍♂️

--- script.js
+++ script.js
@@ -8,6 +8,171 @@
 
 // A project by Simon Freund with code by Marcello Curto
 
+// Default setup
+let cardsSelected = '6'
+let modeSelected = 'single'
+let deckSelected = 'p'
+let deckName = 'portfolio'
+
+// Check for different configuration
+const newString = location.pathname.replace('/', '').split('-')
+cardsSelected = newString[0] ? newString[0] : cardsSelected
+modeSelected = newString[1] ? newString[1] : modeSelected
+deckSelected = newString[2] ? newString[2] : deckSelected
+
+const imageSizes = [480, 640]
+const suffixImage = '.jpg'
+
+const pathFrontCard = './images/cards/front/simon_freund_pairs'
+const altTextFrontCard = 'pairs - verdeckt'
+let pathBackCard = './images/cards/portfolio/simon_freund_pairs_portfolio_'
+const altTextBackCard = 'pairs - aufgedeckt'
+
+let cardsASuffix = ''
+let cardsBSuffix = ''
+let maxNumbersOfRows = 8
+let maxNumbersOfMotifs = 37
+
+if (deckSelected === 'p') {
+    pathBackCard = './images/cards/portfolio/simon_freund_pairs_portfolio_'
+    maxNumbersOfRows = 8
+    maxNumbersOfMotifs = 37
+    cardsASuffix = ''
+    cardsBSuffix = ''
+    deckName = 'portfolio'
+}
+if (deckSelected === 'a') {
+    pathBackCard = './images/cards/allipossess/simon_freund_pairs_allipossess_'
+    maxNumbersOfRows = 24
+    maxNumbersOfMotifs = 329
+    cardsASuffix = ''
+    cardsBSuffix = ''
+    deckName = 'allipossess.com'
+}
+if (deckSelected === 'm') {
+    pathBackCard = './images/cards/mitoderohne/simon_freund_mit_oder_ohne_'
+    maxNumbersOfRows = 8
+    maxNumbersOfMotifs = 32
+    cardsASuffix = '_A'
+    cardsBSuffix = '_B'
+    deckName = 'mit oder ohne'
+}
+
+// Build scoreboard based on mode
+const scoreboardElement = document.getElementById('scoreboard')
+if (modeSelected === 'duel') {
+    // Player 1 Elements
+    const player1ScoreWrapper = document.createElement('div')
+    player1ScoreWrapper.id = 'player-1'
+    player1ScoreWrapper.classList.add('active-player')
+
+    const player1Name = document.createElement('div')
+    player1Name.id = 'scoreboard-player-1'
+    player1Name.contentEditable = true
+    player1Name.innerText = 'PLAYER A'
+
+    const player1ScoreOri = document.createElement('div')
+    player1ScoreOri.id = 'scoreboard-player-1-score'
+    player1ScoreOri.innerText = '0'
+
+    player1ScoreWrapper.appendChild(player1Name)
+    player1ScoreWrapper.appendChild(player1ScoreOri)
+
+    // Player 2 Elements
+    const player2ScoreWrapper = document.createElement('div')
+    player2ScoreWrapper.id = 'player-2'
+
+    const player2Name = document.createElement('div')
+    player2Name.id = 'scoreboard-player-2'
+    player2Name.contentEditable = true
+    player2Name.innerText = 'PLAYER B'
+
+    const player2ScoreOri = document.createElement('div')
+    player2ScoreOri.id = 'scoreboard-player-2-score'
+    player2ScoreOri.innerText = '0'
+
+    player2ScoreWrapper.appendChild(player2Name)
+    player2ScoreWrapper.appendChild(player2ScoreOri)
+
+    // Add players to scoreboard
+    scoreboardElement.appendChild(player1ScoreWrapper)
+    scoreboardElement.appendChild(player2ScoreWrapper)
+
+} else {
+    // Single player mode
+    const singleModePlayerScore = document.createElement('div')
+    singleModePlayerScore.id = 'scoreboard-score'
+    singleModePlayerScore.innerText = '0'
+
+    const singleModePlayerTimer = document.createElement('div')
+    singleModePlayerTimer.id = 'scoreboard-timer'
+    singleModePlayerTimer.innerText = '00:00'
+
+    scoreboardElement.appendChild(singleModePlayerScore)
+    scoreboardElement.appendChild(singleModePlayerTimer)
+}
+
+// Set up board
+const boardgameElement = document.getElementById('boardgame')
+const numberOfRowsSelected = parseInt(cardsSelected, 10)
+const numberOfRows = numberOfRowsSelected > maxNumbersOfRows ? maxNumbersOfRows : numberOfRowsSelected
+const evenNumberOnly = numberOfRows % 2 === 0 ? numberOfRows : numberOfRows + 1
+const numberOfMotifs = (evenNumberOnly * evenNumberOnly) / 2
+
+boardgameElement.style.gridTemplateColumns = `repeat(${evenNumberOnly}, 1fr)`
+
+const randomNumbers = new Array(maxNumbersOfMotifs).fill().map((a, i) => a = i + 1).sort(() => Math.random() - 0.5)
+
+for (let indexImages = 1; indexImages <= numberOfMotifs; indexImages++) {
+    // Create Card Element 1
+    const cardElement1 = document.createElement('div')
+    cardElement1.classList.add('card')
+    cardElement1.dataset.img = 'img-' + indexImages
+
+    // Create Card Element 2
+    const cardElement2 = document.createElement('div')
+    cardElement2.classList.add('card')
+    cardElement2.dataset.img = 'img-' + indexImages
+
+    // Create Card Front Element 1
+    const cardFrontElement1 = document.createElement('img')
+    cardFrontElement1.classList.add('card-front')
+    cardFrontElement1.alt = altTextFrontCard
+    cardFrontElement1.src = pathFrontCard + suffixImage
+    cardFrontElement1.srcset = pathFrontCard + '_' + imageSizes[0] + suffixImage + ' ' + imageSizes[0] + 'w, ' + pathFrontCard + '_' + imageSizes[1] + suffixImage + ' ' + imageSizes[1] + 'w'
+
+    // Create Card Front Element 2
+    const cardFrontElement2 = document.createElement('img')
+    cardFrontElement2.classList.add('card-front')
+    cardFrontElement2.alt = altTextFrontCard
+    cardFrontElement2.src = `${pathFrontCard}.jpg`
+    cardFrontElement2.srcset = pathFrontCard + '_' + imageSizes[0] + suffixImage + ' ' + imageSizes[0] + 'w, ' + pathFrontCard + '_' + imageSizes[1] + suffixImage + ' ' + imageSizes[1] + 'w'
+
+    // Create Card Back Element 1
+    const cardBackElement1 = document.createElement('img')
+    cardBackElement1.classList.add('card-back')
+    cardBackElement1.alt = altTextBackCard
+    cardBackElement1.src = pathBackCard + randomNumbers[indexImages] + cardsASuffix + suffixImage
+    cardBackElement1.srcset = pathBackCard + randomNumbers[indexImages] + cardsASuffix + '_' + imageSizes[0] + suffixImage + ' ' + imageSizes[0] + 'w, ' + pathBackCard + randomNumbers[indexImages] + cardsASuffix + '_' + imageSizes[1] + suffixImage + ' ' + imageSizes[1] + 'w'
+
+    // Create Card Back Element 2
+    const cardBackElement2 = document.createElement('img')
+    cardBackElement2.classList.add('card-back')
+    cardBackElement2.alt = altTextBackCard
+    cardBackElement2.src = pathBackCard + randomNumbers[indexImages] + cardsBSuffix + suffixImage
+    cardBackElement2.srcset = pathBackCard + randomNumbers[indexImages] + cardsBSuffix + '_' + imageSizes[0] + suffixImage + ' ' + imageSizes[0] + 'w, ' + pathBackCard + randomNumbers[indexImages] + cardsBSuffix + '_' + imageSizes[1] + suffixImage + ' ' + imageSizes[1] + 'w'
+
+    // Add elements to board
+    cardElement1.appendChild(cardFrontElement1)
+    cardElement1.appendChild(cardBackElement1)
+    cardElement2.appendChild(cardFrontElement2)
+    cardElement2.appendChild(cardBackElement2)
+
+    boardgameElement.appendChild(cardElement1)
+    boardgameElement.appendChild(cardElement2)
+
+}
+
 // Get all the card elements and put them into an array
 const cards = [...document.getElementsByClassName("card")]
 const duelMode = document.getElementById('player-2')
@@ -38,9 +203,6 @@ let player2Active = false
 let timerID = 0
 let timerRunning = false
 
-let galleryIntervalID = 0
-let galleryRunning = false
-
 let hasFlippedCard = false
 let lockBoard = false
 let firstCard = false
@@ -53,7 +215,6 @@ cards.forEach(card => {
     let randomNumber = Math.floor(Math.random() * numberOfElements)
     card.style.order = randomNumber
 })
-
 
 function flipCard() {
     if (!timerRunning && timerCounter) { timerID = startTimer(); timerRunning = true }
@@ -92,15 +253,17 @@ function disableCards() {
         // If all cards have been turned, end the game
         if (successEvents === numberOfUniqueCards) {
             clearInterval(timerID)
+            emojiRain()
             removeBoard()
-            addGallery()
+            addPlaque()
             return
         }
     }
     if (duelMode) {
         if ((player1Score + player2Score) === numberOfElements / 2) {
+            emojiRain()
             removeBoard()
-            addGallery()
+            addPlaque()
             return
         }
     }
@@ -173,118 +336,32 @@ let timerLogic = () => {
     timerCounter.textContent = minutesAsString + ":" + secondsAsString
 }
 
-let galleryImageOrder = -1
-let galleryImageType = 'A'
-
-// Array with URLs of the gallery
-let galleryImageURLs = []
-
-// Create URLs to use in the gallery
-function createGalleryImageURLs() {
-    for (let index = 1; index < 33; index++) {
-        let imageNumber = 1
-        if (index > 9) { imageNumber = index }
-        else { imageNumber = '0' + index.toString() }
-        const imageASrc = './images/gallery/simon_freund_mit_oder_ohne_Polaroid_' + imageNumber + '_A_640.jpg'
-        const imageBSrc = './images/gallery/simon_freund_mit_oder_ohne_Polaroid_' + imageNumber + '_B_640.jpg'
-        galleryImageURLs.push(imageASrc)
-        galleryImageURLs.push(imageBSrc)
-    }
-}
-
-createGalleryImageURLs()
-
-function waitForImagesToLoadClick() {
-    document.getElementById('img-gallery').onload = () => {
-        nextImage()
-    }
-}
-
-function waitForImagesToLoadAutomatic() {
-    document.getElementById('img-gallery').onload = () => {
-        nextImageAutomatic()
-    }
-}
-
-function nextImage() {
-    console.log('next start', galleryImageOrder);
-    galleryImageOrder++
-    if (galleryRunning) { galleryRunning = false; clearInterval(galleryIntervalID) }
-    if (galleryImageOrder > 63) { galleryImageOrder = 0 }
-    document.getElementById('img-gallery').src = galleryImageURLs[galleryImageOrder]
-    console.log('before plus', galleryImageOrder);
-
-    console.log('after plus', galleryImageOrder);
-}
-
-function prevImage() {
-    console.log('prev start', galleryImageOrder);
-    galleryImageOrder--
-    if (galleryRunning) { galleryRunning = false; clearInterval(galleryIntervalID) }
-    if (galleryImageOrder > 63) { galleryImageOrder = 0 }
-    if (galleryImageOrder < 0) { galleryImageOrder = 63 }
-    document.getElementById('img-gallery').src = galleryImageURLs[galleryImageOrder]
-    console.log('before minus', galleryImageOrder);
-
-    console.log('after minus', galleryImageOrder);
-
-}
-
-function nextImageAutomatic() {
-    if (galleryImageOrder > 63) { galleryImageOrder = 0 }
-    galleryImageOrder++
-    document.getElementById('img-gallery').src = galleryImageURLs[galleryImageOrder]
-
-}
-
 // Clear the scoreboard to make place for the gallery
 const removeBoard = () => {
-    const boardgameElement = document.getElementById('boardgame')
-    const descriptionElement = document.getElementById('description')
     setTimeout(() => { boardgameElement.remove() }, 1000)
-    setTimeout(() => { descriptionElement.remove() }, 2000)
 }
 
-function addGallery() {
-    // Create Gallery Wrapper 
-    const newGalleryWrapper = document.createElement('div')
-    newGalleryWrapper.classList.add('gallery-wrapper')
-    newGalleryWrapper.style.display = 'none'
+// Add plaque after ending the game
+const addPlaque = () => {
+    // Wrapper Element
+    const plaqueWrapperElement = document.createElement('div')
+    plaqueWrapperElement.id = 'plaque-wrapper'
 
-    // Create Image Wrapper
-    const newImageWrapper = document.createElement('div')
-    newImageWrapper.classList.add('image-wrapper')
+    // Emoji Element
+    const plaqueEmoji = document.createElement('p')
+    plaqueEmoji.id = 'plaque-emoji'
+    plaqueEmoji.innerText = '👻'
 
-    // Create Gallery Navigation Elements
-    const newGalleryNavLeft = document.createElement('div')
-    newGalleryNavLeft.classList.add('gallery-navigation-left')
+    // Text Element
+    const plaqueText = document.createElement('p')
+    plaqueText.id = 'plaque-text'
+    plaqueText.innerText = `Congratulations. You've finished the deck ${deckName} with ${evenNumberOnly * evenNumberOnly} cards.`
 
-    const newGalleryNavRight = document.createElement('div')
-    newGalleryNavRight.classList.add('gallery-navigation-right')
+    plaqueWrapperElement.appendChild(plaqueEmoji)
+    plaqueWrapperElement.appendChild(plaqueText)
 
-
-    // Create Image Element
-    const newGalleryImage = document.createElement('img')
-    newGalleryImage.classList.add('gallery-image')
-    newGalleryImage.id = 'img-gallery'
-    newGalleryImage.src = galleryImageURLs[galleryImageOrder]
-
-    document.body.appendChild(newGalleryWrapper)
-    newGalleryWrapper.appendChild(newImageWrapper)
-    newGalleryWrapper.appendChild(newGalleryNavLeft)
-    newGalleryWrapper.appendChild(newGalleryNavRight)
-    newImageWrapper.appendChild(newGalleryImage)
-
-    galleryIntervalID = startGalleryInterval()
-    galleryRunning = true
-
-    setTimeout(() => { newGalleryWrapper.style.display = 'flex' }, 3000)
-
-    newGalleryNavRight.addEventListener('click', nextImage)
-    newGalleryNavLeft.addEventListener('click', prevImage)
+    document.body.appendChild(plaqueWrapperElement)
 }
-
-let startGalleryInterval = () => setInterval(nextImageAutomatic, 1000)
 
 // Listen for clicks on card and then execute flipCard function
 cards.forEach(cardClicked => cardClicked.addEventListener('click', flipCard))
@@ -293,10 +370,6 @@ cards.forEach(cardClicked => cardClicked.addEventListener('click', flipCard))
 const overlayMenu = document.getElementById('overlay-menu')
 function openMenu() { overlayMenu.style.display = 'flex' }
 function closeMenu() { overlayMenu.style.display = 'none' }
-
-// Select the game to be played
-let cardsSelected = '36'
-let modeSelected = 'single'
 
 const playGame = document.getElementById('scoreboard-sub-menu-play')
 if (playGame) {
@@ -308,16 +381,12 @@ if (playGame) {
     const arraySelectCards = [select64, select36, select16]
 
     arraySelectCards.forEach(selection => {
-        if (selection.dataset.cards === cardsSelected) {
-            selection.style.color = 'black'
-        }
+        if (selection.dataset.cards === cardsSelected) { selection.style.color = 'black' }
         selection.addEventListener('click', () => {
             cardsSelected = selection.dataset.cards
             selection.style.color = 'black'
             arraySelectCards.forEach(element => {
-                if (selection != element) {
-                    element.style.color = 'blue'
-                }
+                if (selection != element) { element.style.color = 'blue' }
             })
         })
     })
@@ -329,16 +398,29 @@ if (playGame) {
     const arraySelectMode = [selectSingleMode, selectDuelMode]
 
     arraySelectMode.forEach(selection => {
-        if (selection.dataset.mode === modeSelected) {
-            selection.style.color = 'black'
-        }
+        if (selection.dataset.mode === modeSelected) { selection.style.color = 'black' }
         selection.addEventListener('click', () => {
             modeSelected = selection.dataset.mode
             selection.style.color = 'black'
             arraySelectMode.forEach(element => {
-                if (selection != element) {
-                    element.style.color = 'blue'
-                }
+                if (selection != element) { element.style.color = 'blue' }
+            })
+        })
+    })
+
+    // Select deck
+    const selectMit = document.getElementById('select-m')
+    const selectAll = document.getElementById('select-a')
+    const selectPort = document.getElementById('select-p')
+    const arrayDeck = [selectMit, selectAll, selectPort]
+
+    arrayDeck.forEach(selection => {
+        if (selection.dataset.deck === deckSelected) { selection.style.color = 'black' }
+        selection.addEventListener('click', () => {
+            deckSelected = selection.dataset.deck
+            selection.style.color = 'black'
+            arrayDeck.forEach(element => {
+                if (selection != element) { element.style.color = 'blue' }
             })
         })
     })
@@ -346,14 +428,79 @@ if (playGame) {
     // Start game
     const playButton = document.getElementById('scoreboard-sub-menu-play')
     playButton.addEventListener('click', () => {
-        if (modeSelected === 'single') {
-            if (cardsSelected != '36') {
-                playButton.href = `/${cardsSelected}`
-            }
-        }
-        if (modeSelected === 'duel') {
-            playButton.href = `/${modeSelected}-${cardsSelected}`
-        }
+        playButton.href = `/${cardsSelected}-${modeSelected}-${deckSelected}`
     })
+}
 
+function emojiRain() {
+    // Original code by Robert Heiser & Rachel Smith. Modified by Marcello Curto.
+    const animateWrapper = document.createElement('div')
+    animateWrapper.id = 'animate-wrapper'
+    document.body.appendChild(animateWrapper)
+
+    const animateCanvas = document.createElement('div')
+    animateCanvas.id = 'animate-canvas'
+    animateWrapper.appendChild(animateCanvas)
+
+    const animateObject = document.createElement('div')
+    animateObject.id = 'animate-object'
+    animateCanvas.appendChild(animateObject)
+
+    const emoji = ['👻'];
+    let circles = [];
+
+    for (let i = 0; i < 15; i++) {
+        addCircle(i * 150, [10 + 0, 300], emoji[Math.floor(Math.random() * emoji.length)])
+        addCircle(i * 150, [10 + 0, -300], emoji[Math.floor(Math.random() * emoji.length)])
+        addCircle(i * 150, [10 - 200, -300], emoji[Math.floor(Math.random() * emoji.length)])
+        addCircle(i * 150, [10 + 200, 300], emoji[Math.floor(Math.random() * emoji.length)])
+        addCircle(i * 150, [10 - 400, -300], emoji[Math.floor(Math.random() * emoji.length)])
+        addCircle(i * 150, [10 + 400, 300], emoji[Math.floor(Math.random() * emoji.length)])
+        addCircle(i * 150, [10 - 600, -300], emoji[Math.floor(Math.random() * emoji.length)])
+        addCircle(i * 150, [10 + 600, 300], emoji[Math.floor(Math.random() * emoji.length)])
+    }
+
+    function addCircle(delay, range, color) {
+        setTimeout(function () {
+            let c = new Circle(range[0] + Math.random() * range[1], 80 + Math.random() * 4, color, {
+                x: -0.15 + Math.random() * 0.3,
+                y: 1 + Math.random() * 1
+            }, range)
+            circles.push(c)
+        }, delay)
+    }
+
+    function Circle(x, y, c, v, range) {
+        let _this = this
+        this.x = x
+        this.y = y
+        this.color = c
+        this.v = v
+        this.range = range
+        this.element = document.createElement('span')
+        this.element.style.opacity = 0
+        this.element.style.position = 'absolute'
+        this.element.style.fontSize = '26px'
+        this.element.style.color = 'hsl(' + (Math.random() * 360 | 0) + ',80%,50%)'
+        this.element.innerHTML = c
+        animateObject.appendChild(this.element)
+
+        this.update = function () {
+            _this.y += _this.v.y
+            _this.x += _this.v.x
+            this.element.style.opacity = 1
+            this.element.style.transform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)'
+            this.element.style.webkitTransform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)'
+            this.element.style.mozTransform = 'translate3d(' + _this.x + 'px, ' + _this.y + 'px, 0px)'
+        }
+    }
+
+    function animate() {
+        for (let i in circles) {
+            circles[i].update()
+        }
+        requestAnimationFrame(animate)
+    }
+
+    animate()
 }
